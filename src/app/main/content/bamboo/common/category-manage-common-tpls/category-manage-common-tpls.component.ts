0cd82b1a6d1d961cd5bfb1f60b5f902bdac87913
leon <leon@damaozhu.com>
2018-06-05 16:52:21 +0800

    即将添加分类树模版页面

---
+++ src/app/main/content/bamboo/common/category-manage-common-tpls/category-manage-common-tpls.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-category-manage-common-tpls',
+  templateUrl: './category-manage-common-tpls.component.html',
+  styleUrls: ['./category-manage-common-tpls.component.scss']
+})
+export class CategoryManageCommonTplsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
