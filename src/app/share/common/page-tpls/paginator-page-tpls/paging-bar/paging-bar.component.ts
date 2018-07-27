e046410f7ed89a20a86942526caef03c9317d42a
leon <leon@damaozhu.com>
2018-07-27 19:15:51 +0800

    添加分页栏语言切换功能

--- src/app/share/common/page-tpls/paginator-page-tpls/paging-bar/paging-bar.component.ts
+++ src/app/share/common/page-tpls/paginator-page-tpls/paging-bar/paging-bar.component.ts
@@ -1,23 +1,25 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
 import { MatCheckbox, MatPaginator } from '@angular/material';
 import { PaginatorCommonMdService } from '../paginator-common-md.service';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { IPageChangeParam } from '../paginator-refers';
+import { TranslateService } from '../../../../../../../node_modules/@ngx-translate/core';
 
 @Component({
   selector: 'app-paginator-paging-bar',
   templateUrl: './paging-bar.component.html',
   styleUrls: ['./paging-bar.component.scss']
 })
-export class PagingBarComponent implements OnInit {
+export class PagingBarComponent implements OnInit, OnDestroy {
+
 
   selected = false;
   viewMode = true;
   @ViewChild(MatCheckbox) allCheckCt: MatCheckbox;
   @ViewChild(MatPaginator) paginatorCt: MatPaginator;
   destroy$ = new Subject<boolean>();
-  constructor(public mdSrv: PaginatorCommonMdService) {
+  constructor(public mdSrv: PaginatorCommonMdService, private tranSrv: TranslateService) {
     //订阅查看|选择模式
     this.mdSrv.selectMode$.pipe(takeUntil(this.destroy$)).subscribe(selectMode => {
       this.onSelectModelChange(selectMode);
@@ -25,8 +27,15 @@ export class PagingBarComponent implements OnInit {
   }//constructor
 
   ngOnInit() {
-
+    this.tranSrv.get('tips.ItemsPerPageLabel').subscribe(msg => {
+      this.paginatorCt._intl.itemsPerPageLabel = msg;
+    });
   }//ngOnInit
+
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
+  }//ngOnDestroy
 
   pageChange(evt: IPageChangeParam) {
     this.mdSrv.pageParam = evt;
