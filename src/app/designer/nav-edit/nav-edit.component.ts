e1040724e1f2c57b239a8119ba3dad74c6734c7d
leon <leon@damaozhu.com>
2018-08-14 19:16:17 +0800

    完成空白导航页面编写

--- src/app/designer/nav-edit/nav-edit.component.ts
+++ src/app/designer/nav-edit/nav-edit.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Subscription } from '../../../../node_modules/rxjs';
+import { ObservableMedia } from '../../../../node_modules/@angular/flex-layout';
+import { MatDrawer } from '../../../../node_modules/@angular/material';
 
 @Component({
   selector: 'app-nav-edit',
@@ -7,9 +10,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavEditComponent implements OnInit {
 
-  constructor() { }
+  titleName = 'nav.NavSetting';
+  iconName = 'chrome_reader_mode'
+  showDrawer = true;//drawer显示状态 true=>屉子现在为显示状态
+  drawerOnSwitch = false;
+  sideMode = 'side';
+  watcher: Subscription;
+  drawerDyStyle = {};
+  @ViewChild('drawer', { read: MatDrawer }) drawer: MatDrawer;
+  constructor(public media: ObservableMedia) {
+
+  }//constructor
 
   ngOnInit() {
-  }
+    this.watcher = this.media.subscribe(() => {
+      let largeScreen = this.media.isActive('gt-xs');
+
+      //当屉子当前处于打开,但是屏幕改变为小屏幕,需要先隐藏屉子
+      if (this.showDrawer && !largeScreen)
+        this.drawer.close();
+      this.drawerOnSwitch = false;
+      this.showDrawer = largeScreen;
+
+      if (largeScreen)
+        this.sideMode = 'side';
+      else
+        this.sideMode = 'push';
+
+
+      this.showDrawer = largeScreen;
+      let drawerWidth = '75%';
+      if (this.showDrawer) {
+        this.drawer.open();
+        drawerWidth = '50%'
+      }
+      this.drawerDyStyle = { width: drawerWidth };
+    });
+  }//ngOnInit
 
 }
