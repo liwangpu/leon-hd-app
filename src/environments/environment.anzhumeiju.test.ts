2389b8ecff68d30cad5c0ccc913686b1324b78d7
leon <leon@damaozhu.com>
2018-08-20 19:16:34 +0800

    添加安住美居部署环境

---
+++ src/environments/environment.anzhumeiju.test.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: true,
+  hmr: false,
+  serveBase: 'http://testapi.decohero.net',
+  secretKey:'dmz-anzhumeiju-app',
+  shareServerBase: 'http://testshare.decohero.net',
+  dialogMin: {
+    width: '450px',
+    height: '600px'
+  },
+  dialogMed: {
+    width: '600px',
+    height: '750px'
+  }
+};
---
+++ src/environments/environment.anzhumeiju.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: true,
+  hmr: false,
+  serveBase: 'http://api.decohero.net',
+  secretKey:'dmz-anzhumeiju-app',
+  shareServerBase: 'http://share.decohero.net',
+  dialogMin: {
+    width: '450px',
+    height: '600px'
+  },
+  dialogMed: {
+    width: '600px',
+    height: '750px'
+  }
+};
