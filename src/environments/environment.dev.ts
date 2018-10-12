9d22eeb925c4ba223a360051d623cd5f47b37ade
leon <leon@damaozhu.com>
2018-10-12 11:51:49 +0800

    修改启用ali服务器作为开发服务器(临时)

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,7 +1,8 @@
 export const environment = {
     production: false,
     hmr: false,
-    serveBase: 'http://192.168.1.176:1882',
+    serveBase: 'http://dmz-api-pucst.com',
+    // serveBase: 'http://192.168.1.176:1882',
     secretKey:'damaozhu-app',
     shareServerBase: 'http://192.168.1.3:82/share-resource',
     dialogMin: {
